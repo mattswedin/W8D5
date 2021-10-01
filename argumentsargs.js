b820a1245179159a3413945f33abc46d3a4537d1
Matthew Swedin <matthew.swedin@gmail.comgit config --global user.email matthew.swedin@gmail.com>
2021-10-01 16:04:19 -0400

    completed currys

--- argumentsargs.js
+++ argumentsargs.js
@@ -8,15 +8,15 @@
 
 // };
 
-// function sum(...args) {
-//     let sumd = 0;
-//     for (let i = 0; i < args.length; i++) {
-//         const element = args[i];
-//         sumd += element;
-//     }
-//     return sumd;
+function sum(...args) {
+    let sumd = 0;
+    for (let i = 0; i < args.length; i++) {
+        const element = args[i];
+        sumd += element;
+    }
+    return sumd;
 
-// };
+};
 
 // console.log(sum(1, 2, 3, 4) === 10);
 // console.log(sum(1, 2, 3, 4, 5) === 15);
@@ -106,46 +106,66 @@ Function.prototype.curry = function () {
             }
         }
     }
-}
+};
 
 
-function sumThree(num1, num2, num3) {
-    return num1 + num2 + num3;
-}
+// function sumThree(num1, num2, num3) {
+//     return num1 + num2 + num3;
+// }
 
-sumThree(4, 20, 6); // == 30
+// sumThree(4, 20, 6); // == 30
 
-// you'll write `Function#curry`!
-let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
-f1 = f1(4); // [Function]
-f1 = f1(20); // [Function]
-f1 = f1(6); // = 30
+// // you'll write `Function#curry`!
+// let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+// f1 = f1(4); // [Function]
+// f1 = f1(20); // [Function]
+// f1 = f1(6); // = 30
 
-// or more briefly:
-console.log(sumThree.curry(3)(4)(20)(6)); // == 30
+// // or more briefly:
+// console.log(sumThree.curry(3)(4)(20)(6)); // == 30
 
 
-function curriedSum(numArgs) {
-    let numbers = [];
-    function _curriedSum(num) {
-        numbers.push(num);
-        if (numbers.length === numArgs) {
-            let numSum = 0;
-            for (let i = 0; i < numbers.length; i++) {
-                const element = numbers[i];
-                numSum += element;
-            }
-            return numSum;
+// function curriedSum(numArgs) {
+//     let numbers = [];
+//     function _curriedSum(num) {
+//         numbers.push(num);
+//         if (numbers.length === numArgs) {
+//             let numSum = 0;
+//             for (let i = 0; i < numbers.length; i++) {
+//                 const element = numbers[i];
+//                 numSum += element;
+//             }
+//             return numSum;
+//         } else {
+//             return _curriedSum;
+//         }
+
+//     }
+//     return _curriedSum;
+// }
+
+// const sum = curriedSum(4);
+// console.log(sum(5)(30)(20)(1)); // => 56
+
+// const sum2 = curriedSum(6);
+// console.log(sum2(1)(1)(1)(1)(1)(1)); // => 6
+
+Function.prototype.curry = function(numArgs){
+
+    let argsSoFar = [];
+    let that = this;
+
+    function _curriedSum(arg) {
+        argsSoFar.push(arg);
+        if (argsSoFar.length === numArgs) {
+            return that.apply(null, argsSoFar);
         } else {
             return _curriedSum;
         }
 
     }
     return _curriedSum;
-}
 
-const sum = curriedSum(4);
-console.log(sum(5)(30)(20)(1)); // => 56
+};
 
-const sum2 = curriedSum(6);
-console.log(sum2(1)(1)(1)(1)(1)(1)); // => 6
+console.log(sum.curry(3)(4)(20)(6))
